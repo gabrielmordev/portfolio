9b6951fb2d6c097116af9b7b74d1389755f836f2
gabrielmordev <gabrielmordev@gmail.com>
2025-06-17 13:13:15 +0200

    corregido build para cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Asegúrate de que esto sea false o elimínalo
+  // devIndicators: false
 };
-devIndicators: false
+
 export default nextConfig;
+
