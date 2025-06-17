234a4c5b54ea76df33da5a7b593b66bcd15d78f2
gabrielmordev <gabrielmordev@gmail.com>
2025-06-17 13:23:10 +0200

    prueba

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Deshabilitar la caché de webpack para producción
-    if (!dev) {
-      config.cache = {
-        type: 'memory'
-      };
-    }
-    return config;
-  },
 };
-
+devIndicators: false
 export default nextConfig;
-
 
